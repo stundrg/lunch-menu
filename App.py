46fbae95fad37ea7dcef3655386eb01262a2c73d
stundrg <khr0106@naver.com>
2025-02-04 11:54:49 +0900

    배고픈 점심

---
+++ App.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+ 
+st.write("""
+# ~ 내가 제일 잘먹어 ~
+
+         배고프다...
+
+![img](https://cdnweb01.wikitree.co.kr/webdata/editor/201608/14/img_20160814095841_398f23f1.jpg)
+""" )
+
+df = pd.read_csv('note/menu.csv')
+
+start_idx = df.columns.get_loc('2025-01-07')
+melted_df = df.melt(id_vars=['ename'],
+value_vars=df.columns[start_idx:-2],
+                        var_name='dt',value_name = 'menu')
+
+not_na_df = melted_df[~melted_df['menu'].isin(['-','x','<결석>'])]
+gdf = not_na_df.groupby('ename')['menu'].count().reset_index()
+#gdf.plot(x='ename', y='menu'. kind = 'bar'
+
+# 📊 Matplotlib로 바 차트 그리기
+
+fig, ax = plt.subplots()
+gdf.plot(x='ename',y = 'menu', kind = 'bar', ax=ax)
+st.pyplot(fig)
