c9ea46471b029640149a5ccfa5dce196acf58ba4
stundrg <khr0106@naver.com>
2025-02-06 09:07:47 +0900

    pdm update

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
 st.markdown("# Main page (oﾟvﾟ)ノ ")
+[img](https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.naver.com%2Fondo_h%2F221500940078%3FviewType%3Dpc&psig=AOvVaw1yIJWuiXT1OqAvmAFM93Pn&ust=1738850261366000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKDXxKPYrIsDFQAAAAAdAAAAABAE)
+
 st.sidebar.markdown("# Main page 🎈")
